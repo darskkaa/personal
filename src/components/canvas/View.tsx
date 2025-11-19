ecec95f2e9c7520f19c0f10302d6b645122d1810
darskkaa <johnfdark@duck.com>
2025-11-18 20:34:08 -0500

    Fix TypeScript error in View component

--- src/components/canvas/View.tsx
+++ src/components/canvas/View.tsx
@@ -4,7 +4,7 @@ import { forwardRef, Suspense, useImperativeHandle, useRef } from "react";
 import { OrbitControls, PerspectiveCamera, View as ViewImpl } from "@react-three/drei";
 
 export const View = forwardRef(({ children, ...props }: any, ref) => {
-    const localRef = useRef(null);
+    const localRef = useRef<HTMLDivElement>(null!);
     useImperativeHandle(ref, () => localRef.current);
 
     return (
